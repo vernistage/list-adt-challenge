e3de314124308f6efcb2db91995208363a997148
vernistage <sdickers10@gmail.com>
2017-03-06 22:08:16 -0600

    initial commit

--- lib/array_list.rb
+++ lib/array_list.rb
@@ -2,5 +2,27 @@ require_relative 'fixed_array'
 require_relative 'exceptions'
 
 class ArrayList
+  #How to Initialize and store initial data?
+  def initialize()
+  end
+
+  #How to add data to fixed array?
+  def add(element)
+  end
+
+  def get(position)
+  end
+
+  def set(position, element)
+  end
+
+  def first()
+  end
+
+  def last()
+  end
+
+  def length()
+  end
 
 end
